e3c46745df56340fd18e60e67a286b91108f4a51
rahat2020 <bloodyhell4040@gmail.com>
2024-11-15 21:10:23 +0600

    necessary components and tailwind configs and hooks are implemented

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import Topbar from './Topbar'
+
+const HomeComponents = () => {
+  return (
+    <div>
+        <Topbar/>
+    </div>
+  )
+}
+
+export default HomeComponents
---
+++ src/data/navData.js
@@ -0,0 +1,35 @@
+import { ChevronDown } from "react-feather";
+
+export const navData = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Products",
+    href: "/products"
+  },
+  {
+    label: "Specialties",
+    href: "#",
+    icon: <ChevronDown />, 
+    dropdown: [
+      { label: "Cardiologist", href: "/cardiologist" },
+      { label: "Neurology", href: "/neurology" },
+      { label: "Ophthalmology", href: "/ophthalmology" },
+      { label: "Urology", href: "/urology" },
+    ],
+  },
+  {
+    label: "About Us",
+    href: "/about",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+  {
+    label: "Contact Us",
+    href: "/contacts",
+  },
+];
